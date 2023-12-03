ffa0c18e88a96469faf1f207d8a8888f9333303b
angelacao2109 <angelacao2109@yahoo.com>
2023-12-02 20:40:41 -0500

    Initial Commit

---
+++ Random Recipe Finder/Random_Recipe_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Random_Recipe_FinderApp.swift
+//  Random Recipe Finder
+//
+//  Created by Angela  on 12/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Random_Recipe_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
